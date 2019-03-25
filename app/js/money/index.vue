4809ff53a5cc6ff22af895d4c6ebe6c4f86c746c
jizou <815371297@qq.com>
2019-03-25 20:54:41 +0800

    first commit

---
+++ app/js/money/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <Heador/>
+        <Slide/>
+        <Ada/>
+        <Money/>
+        <Adb/>
+        <Finance/>
+        <Adc/>
+        <Footer/>
+        <Navbar/>
+    </div>
+</template>
+
+<script>
+import Heador from '../public/header.vue'
+import Slide  from './mslider.vue'
+import Ada from './ada.vue'
+import Footer from '../public/footer.vue'
+import Navbar from '../public/navbar.vue'
+import Money from './money.vue'
+import Adb from './adb.vue'
+import Finance from './finance.vue'
+import Adc from './adc.vue'
+export default {
+  components:{
+    Heador,
+    Slide,
+    Ada,
+    Footer,
+    Navbar,
+    Money,
+    Adb,
+    Finance,
+    Adc
+  }
+}
+</script>
